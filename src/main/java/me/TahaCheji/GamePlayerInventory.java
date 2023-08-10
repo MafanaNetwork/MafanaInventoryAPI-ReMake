f23f655e38cb20fb5a12d25910377b0f66bea91c
tahac <wolfprogaming123@gmail.com>
2023-08-11 00:19:18 +0100

    done

---
+++ src/main/java/me/TahaCheji/GamePlayerInventory.java
@@ -0,0 +1,162 @@
+package me.TahaCheji;
+
+import de.tr7zw.nbtapi.NBTItem;
+import me.TahaCheji.objects.DatabaseInventoryData;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class GamePlayerInventory {
+
+    public List<ItemStack> inventoryItems;
+    public List<ItemStack> inventoryArmor;
+    public Player gamePlayer;
+    public OfflinePlayer offlinePlayer;
+
+    public GamePlayerInventory(Player gamePlayer) throws Exception {
+        this.gamePlayer = gamePlayer;
+        DatabaseInventoryData data = Inv.getInstance().getInvMysqlInterface().getData(gamePlayer.getPlayer());
+        if(gamePlayer.getPlayer().isOnline()){
+            List<ItemStack> pInv = new ArrayList<>();
+            for (ItemStack itemStack : gamePlayer.getPlayer().getInventory()){
+                pInv.add(itemStack);
+            }
+            inventoryItems = pInv;
+            inventoryArmor = Arrays.asList(gamePlayer.getPlayer().getInventory().getArmorContents());
+            return;
+        }
+
+        inventoryItems = Arrays.asList(new InventoryDataHandler(Inv.getInstance()).decodeItems(data.getRawInventory()));
+        inventoryArmor = Arrays.asList(new InventoryDataHandler(Inv.getInstance()).decodeItems(data.getRawArmor()));
+    }
+
+    public GamePlayerInventory(OfflinePlayer offlinePlayer) throws Exception {
+        this.offlinePlayer = offlinePlayer;
+        DatabaseInventoryData data = Inv.getInstance().getInvMysqlInterface().getData(offlinePlayer);
+        inventoryItems = Arrays.asList(new InventoryDataHandler(Inv.getInstance()).decodeItems(data.getRawInventory()));
+        inventoryArmor = Arrays.asList(new InventoryDataHandler(Inv.getInstance()).decodeItems(data.getRawArmor()));
+    }
+
+    public List<ItemStack> getInventoryArmor() {
+        return inventoryArmor;
+    }
+
+    public List<ItemStack> getInventoryItems() {
+        return inventoryItems;
+    }
+
+    public void addItem(ItemStack itemStack) {
+        List<ItemStack> itemStacks = getInventoryItems();
+        itemStacks.add(itemStack);
+        setInventoryItems(itemStacks);
+    }
+
+    public void removeItem(ItemStack itemStack) {
+        List<ItemStack> itemStacks = getInventoryItems();
+
+        Iterator<ItemStack> iterator = itemStacks.iterator();
+        while (iterator.hasNext()) {
+            ItemStack item = iterator.next();
+            String itemUUID = new NBTItem(item).getString("GameItemUUID");
+            String targetUUID = new NBTItem(itemStack).getString("GameItemUUID");
+
+            if (itemUUID.equalsIgnoreCase(targetUUID)) {
+                iterator.remove();
+                break;  // Stop after removing the first matching item
+            }
+        }
+
+        setInventoryItems(itemStacks);
+    }
+
+    public void addArmorItem(ItemStack armorItem) {
+        List<ItemStack> armorItems = getInventoryArmor();
+        armorItems.add(armorItem);
+        setInventoryArmor(armorItems);
+    }
+
+    public void removeArmorItem(ItemStack armorItem) {
+        List<ItemStack> armorItems = getInventoryArmor();
+
+        Iterator<ItemStack> iterator = armorItems.iterator();
+        while (iterator.hasNext()) {
+            ItemStack item = iterator.next();
+            String itemUUID = new NBTItem(item).getString("GameItemUUID");
+            String targetUUID = new NBTItem(armorItem).getString("GameItemUUID");
+
+            if (itemUUID.equalsIgnoreCase(targetUUID)) {
+                iterator.remove();
+                break;  // Stop after removing the first matching armor item
+            }
+        }
+
+        setInventoryArmor(armorItems);
+    }
+
+
+    public void setInventoryArmor(List<ItemStack> inventoryArmor) {
+        this.inventoryArmor = inventoryArmor;
+        Player player = gamePlayer.getPlayer();
+        if(player.isOnline()){
+            player.getInventory().setArmorContents(inventoryArmor.toArray(new ItemStack[0]));
+
+            player.updateInventory(); // Update the player's inventory view
+            player.sendMessage(ChatColor.GREEN + "[MafanaInventory Manager]: Your armor has been updated.");
+            return;
+        }
+        saveOfflineInventory(inventoryArmor, getInventoryArmor(), player);
+    }
+
+    public void setInventoryItems(List<ItemStack> inventoryItems) {
+        Player player = gamePlayer.getPlayer();
+        this.inventoryItems = inventoryItems;
+        if(player.isOnline()){
+            player.getInventory().clear(); // Clear the player's current inventory
+
+            for (int i = 0; i < inventoryItems.size(); i++) {
+                ItemStack item = inventoryItems.get(i);
+                player.getInventory().setItem(i, item);
+            }
+
+            player.updateInventory(); // Update the player's inventory view
+            player.sendMessage(ChatColor.GREEN + "[MafanaInventory Manager]: Your inventory has been updated.");
+            return;
+
+        }
+        saveOfflineInventory(inventoryItems, getInventoryArmor(), player);
+    }
+
+    public void setInventoryArmor(List<ItemStack> inventoryArmor, OfflinePlayer player) {
+        this.inventoryArmor = inventoryArmor;
+        saveOfflineInventory(getInventoryItems(), inventoryArmor, player);
+    }
+
+    public void setInventoryItems(List<ItemStack> inventoryItems, OfflinePlayer player) {
+        this.inventoryItems = inventoryItems;
+        saveOfflineInventory(inventoryItems, getInventoryArmor(), player);
+    }
+
+    public void saveOfflineInventory(List<ItemStack> inventoryItems, List<ItemStack> inventoryArmor, OfflinePlayer player) {
+        Bukkit.getScheduler().runTaskLaterAsynchronously(Inv.getInstance(), () -> {
+            try {
+                ItemStack[] inventory = inventoryItems.toArray(new ItemStack[0]);
+                ItemStack[] armor = inventoryArmor.toArray(new ItemStack[0]);
+                Inv.getInstance().getInvMysqlInterface().setData(player,
+                        Inv.getInstance().getInventoryDataHandler().encodeItems(inventory),
+                        Inv.getInstance().getInventoryDataHandler().encodeItems(armor),
+                        "True");
+            } catch (Exception e) {
+                e.printStackTrace(); // Log the exception
+            }
+        }, 2L);
+
+    }
+
+}
---
+++ src/main/java/me/TahaCheji/Inv.java
@@ -0,0 +1,129 @@
+package me.TahaCheji;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+
+import me.TahaCheji.database.InvMysqlInterface;
+import me.TahaCheji.database.MysqlSetup;
+import me.TahaCheji.discordCommand.InventoryCommand;
+import me.TahaCheji.events.DropItem;
+import me.TahaCheji.events.InventoryClick;
+import me.TahaCheji.events.PlayerJoin;
+import me.TahaCheji.events.PlayerQuit;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
+import org.bukkit.Bukkit;
+import org.bukkit.event.HandlerList;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import javax.security.auth.login.LoginException;
+
+public class Inv extends JavaPlugin {
+
+	//Loan the players inventory in a discord bot
+	//change the players inventory while offline
+	
+	public static Logger log;
+	public boolean useProtocolLib = false;
+	public static String pluginName = "MafanaInventoryAPI";
+	//public Set<String> playersSync = new HashSet<String>();
+	public static boolean is19Server = true;
+	public static boolean is13Server = false;
+	public static boolean isDisabling = false;
+	
+	private static ConfigHandler configHandler;
+	private static SoundHandler sH;
+	private static MysqlSetup databaseManager;
+	private static InvMysqlInterface invMysqlInterface;
+	private static InventoryDataHandler idH;
+	private static BackgroundTask bt;
+
+	private static Inv instance;
+	public JDA builder = null;
+	
+	@Override
+    public void onEnable() {
+		String token = "";
+		builder = JDABuilder.createDefault(token)
+				.enableIntents(GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES, GatewayIntent.MESSAGE_CONTENT, GatewayIntent.DIRECT_MESSAGE_REACTIONS
+						,GatewayIntent.GUILD_MESSAGE_REACTIONS)
+				.setMemberCachePolicy(MemberCachePolicy.ALL)
+				.addEventListeners(new InventoryCommand())
+				.build();
+		try {
+			builder.awaitReady();
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+		instance = this;
+		log = getLogger();
+    	configHandler = new ConfigHandler(this);
+    	sH = new SoundHandler(this);
+    	checkDependency();
+    	bt = new BackgroundTask(this);
+    	databaseManager = new MysqlSetup(this);
+    	invMysqlInterface = new InvMysqlInterface(this);
+    	idH = new InventoryDataHandler(this);
+    	//Register Listeners
+    	PluginManager pm = getServer().getPluginManager();
+    	pm.registerEvents(new PlayerJoin(this), this);
+    	pm.registerEvents(new PlayerQuit(this), this);
+    	pm.registerEvents(new DropItem(this), this);
+    	pm.registerEvents(new InventoryClick(this), this);
+    	pm.registerEvents(new InventoryCommand(), this);
+    	log.info(pluginName + " loaded successfully!");
+	}
+	
+	@Override
+    public void onDisable() {
+		isDisabling = true;
+		Bukkit.getScheduler().cancelTasks(this);
+		HandlerList.unregisterAll(this);
+		if (databaseManager.getConnection() != null) {
+			bt.onShutDownDataSave();
+			databaseManager.closeConnection();
+		}
+		log.info(pluginName + " is disabled!");
+		builder.shutdownNow();
+	}
+	
+	public ConfigHandler getConfigHandler() {
+		return configHandler;
+	}
+	public MysqlSetup getDatabaseManager() {
+		return databaseManager;
+	}
+	public InvMysqlInterface getInvMysqlInterface() {
+		return invMysqlInterface;
+	}
+	public SoundHandler getSoundHandler() {
+		return sH;
+	}
+	public BackgroundTask getBackgroundTask() {
+		return bt;
+	}
+	public InventoryDataHandler getInventoryDataHandler() {
+		return idH;
+	}
+	
+	private void checkDependency() {
+		//Check dependency
+        if (Bukkit.getPluginManager().getPlugin("ProtocolLib") != null) {
+        	useProtocolLib = true;
+        	log.info("ProtocolLib dependency found.");
+        } else {
+        	useProtocolLib = false;
+        	log.warning("ProtocolLib dependency not found. No support for modded items NBT data!");
+        }
+	}
+
+	public static Inv getInstance() {
+		return instance;
+	}
+}
